1457711764d6026581419ebf39baaeca91b1a03c
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-12-18 13:02:09 +0100

    feat(auth): implement user registration with model / serializer / endpoint / view

--- quizzly_backend/auth_app/models.py
+++ quizzly_backend/auth_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     def __str__(self) -> str:
