9c09b15844e86d77c900e96368a63f6bce314db6
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-12-21 20:00:05 +0100

    added quiz-app

--- quizzly_backend/settings.py
+++ quizzly_backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'auth_app',
+    'quiz',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django.contrib.admin',
